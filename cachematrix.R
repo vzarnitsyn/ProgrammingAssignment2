a2f468a05858c64e772881264f11b722183e282c
Vladimir Zarnitsyn <vzarnitsyn@gmail.com>
2014-07-26 21:48:07 -0400

    result of Home Work. VZ.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions in this module allows to set the matrix the way which
+## allows an access to cashed inverse matrix
+## It is an exercise  so matrixes are not checked for being inversible
+## rather the sove function is meing applied to whatever matrix is used
 
-## Write a short comment describing this function
+## make Cahse Matrix is a list of 4 functions set, get, setinverse and getinverse
+## as an exercise for cashed results
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Cashe solve return inverse matrix for its argument matrix 
+## It tries to save machine time by returning cashed invers
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinverse(inv)
+  inv
+  
 }
+
